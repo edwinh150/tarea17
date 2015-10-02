363fa1a32f0a9bfd2d1cedeab01db516989ba235
edwinh150 <edwinh2410@gmail.com>
2015-10-02 15:25:44 -0400

    agregando datos como datagridview y button a el formulario consultar

--- RegistroPelicula/RegistrosPelicula/ConsultarForm.Designer.cs
+++ RegistroPelicula/RegistrosPelicula/ConsultarForm.Designer.cs
@@ -28,11 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ResultadodataGridView = new System.Windows.Forms.DataGridView();
+            this.Mostrarbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultadodataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ResultadodataGridView
+            // 
+            this.ResultadodataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultadodataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResultadodataGridView.Location = new System.Drawing.Point(12, 12);
+            this.ResultadodataGridView.Name = "ResultadodataGridView";
+            this.ResultadodataGridView.Size = new System.Drawing.Size(610, 334);
+            this.ResultadodataGridView.TabIndex = 0;
+            // 
+            // Mostrarbutton
+            // 
+            this.Mostrarbutton.Location = new System.Drawing.Point(279, 352);
+            this.Mostrarbutton.Name = "Mostrarbutton";
+            this.Mostrarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Mostrarbutton.TabIndex = 1;
+            this.Mostrarbutton.Text = "Mostrar";
+            this.Mostrarbutton.UseVisualStyleBackColor = true;
+            this.Mostrarbutton.Click += new System.EventHandler(this.Mostrarbutton_Click);
+            // 
+            // ConsultarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 379);
+            this.Controls.Add(this.Mostrarbutton);
+            this.Controls.Add(this.ResultadodataGridView);
+            this.Name = "ConsultarForm";
             this.Text = "ConsultarForm";
+            ((System.ComponentModel.ISupportInitialize)(this.ResultadodataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView ResultadodataGridView;
+        private System.Windows.Forms.Button Mostrarbutton;
     }
 }
--- RegistroPelicula/RegistrosPelicula/ConsultarForm.cs
+++ RegistroPelicula/RegistrosPelicula/ConsultarForm.cs
@@ -16,5 +16,10 @@ namespace RegistroPelicula.RegistrosPelicula
         {
             InitializeComponent();
         }
+
+        private void Mostrarbutton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
