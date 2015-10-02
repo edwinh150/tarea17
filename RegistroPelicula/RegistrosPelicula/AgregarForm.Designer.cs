6909feef377d3d34db3269ee3e2e58ccb830e8a2
edwinh150 <edwinh2410@gmail.com>
2015-10-02 15:12:51 -0400

    agregando textbox , label, button a formulario agregar

--- RegistroPelicula/RegistrosPelicula/AgregarForm.Designer.cs
+++ RegistroPelicula/RegistrosPelicula/AgregarForm.Designer.cs
@@ -28,11 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PeliculaIdtextBox = new System.Windows.Forms.TextBox();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.DescripciontextBox = new System.Windows.Forms.TextBox();
+            this.AnotextBox = new System.Windows.Forms.TextBox();
+            this.CalificaciontextBox = new System.Windows.Forms.TextBox();
+            this.IMDBtextBox = new System.Windows.Forms.TextBox();
+            this.CategoriaIdtextBox = new System.Windows.Forms.TextBox();
+            this.PIlabel = new System.Windows.Forms.Label();
+            this.Nlabel = new System.Windows.Forms.Label();
+            this.Delabel = new System.Windows.Forms.Label();
+            this.Alabel = new System.Windows.Forms.Label();
+            this.Calabel = new System.Windows.Forms.Label();
+            this.CIlabel = new System.Windows.Forms.Label();
+            this.Imlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PeliculaIdtextBox
+            // 
+            this.PeliculaIdtextBox.Location = new System.Drawing.Point(60, 58);
+            this.PeliculaIdtextBox.Name = "PeliculaIdtextBox";
+            this.PeliculaIdtextBox.Size = new System.Drawing.Size(139, 20);
+            this.PeliculaIdtextBox.TabIndex = 0;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Location = new System.Drawing.Point(313, 214);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Guardarbutton.TabIndex = 1;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(60, 104);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(139, 20);
+            this.NombretextBox.TabIndex = 0;
+            // 
+            // DescripciontextBox
+            // 
+            this.DescripciontextBox.Location = new System.Drawing.Point(60, 158);
+            this.DescripciontextBox.Name = "DescripciontextBox";
+            this.DescripciontextBox.Size = new System.Drawing.Size(139, 20);
+            this.DescripciontextBox.TabIndex = 0;
+            // 
+            // AnotextBox
+            // 
+            this.AnotextBox.Location = new System.Drawing.Point(60, 214);
+            this.AnotextBox.Name = "AnotextBox";
+            this.AnotextBox.Size = new System.Drawing.Size(139, 20);
+            this.AnotextBox.TabIndex = 0;
+            // 
+            // CalificaciontextBox
+            // 
+            this.CalificaciontextBox.Location = new System.Drawing.Point(60, 275);
+            this.CalificaciontextBox.Name = "CalificaciontextBox";
+            this.CalificaciontextBox.Size = new System.Drawing.Size(139, 20);
+            this.CalificaciontextBox.TabIndex = 0;
+            // 
+            // IMDBtextBox
+            // 
+            this.IMDBtextBox.Location = new System.Drawing.Point(285, 58);
+            this.IMDBtextBox.Name = "IMDBtextBox";
+            this.IMDBtextBox.Size = new System.Drawing.Size(139, 20);
+            this.IMDBtextBox.TabIndex = 0;
+            // 
+            // CategoriaIdtextBox
+            // 
+            this.CategoriaIdtextBox.Location = new System.Drawing.Point(285, 130);
+            this.CategoriaIdtextBox.Name = "CategoriaIdtextBox";
+            this.CategoriaIdtextBox.Size = new System.Drawing.Size(139, 20);
+            this.CategoriaIdtextBox.TabIndex = 0;
+            // 
+            // PIlabel
+            // 
+            this.PIlabel.AutoSize = true;
+            this.PIlabel.Location = new System.Drawing.Point(60, 39);
+            this.PIlabel.Name = "PIlabel";
+            this.PIlabel.Size = new System.Drawing.Size(56, 13);
+            this.PIlabel.TabIndex = 2;
+            this.PIlabel.Text = "Pelicula Id";
+            // 
+            // Nlabel
+            // 
+            this.Nlabel.AutoSize = true;
+            this.Nlabel.Location = new System.Drawing.Point(57, 88);
+            this.Nlabel.Name = "Nlabel";
+            this.Nlabel.Size = new System.Drawing.Size(44, 13);
+            this.Nlabel.TabIndex = 2;
+            this.Nlabel.Text = "Nombre";
+            // 
+            // Delabel
+            // 
+            this.Delabel.AutoSize = true;
+            this.Delabel.Location = new System.Drawing.Point(60, 142);
+            this.Delabel.Name = "Delabel";
+            this.Delabel.Size = new System.Drawing.Size(63, 13);
+            this.Delabel.TabIndex = 2;
+            this.Delabel.Text = "Descripcion";
+            // 
+            // Alabel
+            // 
+            this.Alabel.AutoSize = true;
+            this.Alabel.Location = new System.Drawing.Point(60, 198);
+            this.Alabel.Name = "Alabel";
+            this.Alabel.Size = new System.Drawing.Size(26, 13);
+            this.Alabel.TabIndex = 2;
+            this.Alabel.Text = "Ano";
+            // 
+            // Calabel
+            // 
+            this.Calabel.AutoSize = true;
+            this.Calabel.Location = new System.Drawing.Point(60, 259);
+            this.Calabel.Name = "Calabel";
+            this.Calabel.Size = new System.Drawing.Size(61, 13);
+            this.Calabel.TabIndex = 2;
+            this.Calabel.Text = "Calificacion";
+            // 
+            // CIlabel
+            // 
+            this.CIlabel.AutoSize = true;
+            this.CIlabel.Location = new System.Drawing.Point(282, 114);
+            this.CIlabel.Name = "CIlabel";
+            this.CIlabel.Size = new System.Drawing.Size(66, 13);
+            this.CIlabel.TabIndex = 2;
+            this.CIlabel.Text = "Categoria ID";
+            // 
+            // Imlabel
+            // 
+            this.Imlabel.AutoSize = true;
+            this.Imlabel.Location = new System.Drawing.Point(282, 42);
+            this.Imlabel.Name = "Imlabel";
+            this.Imlabel.Size = new System.Drawing.Size(106, 13);
+            this.Imlabel.TabIndex = 2;
+            this.Imlabel.Text = "Puntuacion de IMDB";
+            // 
+            // AgregarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "AgregarForm";
+            this.ClientSize = new System.Drawing.Size(484, 317);
+            this.Controls.Add(this.Imlabel);
+            this.Controls.Add(this.CIlabel);
+            this.Controls.Add(this.Calabel);
+            this.Controls.Add(this.Alabel);
+            this.Controls.Add(this.Delabel);
+            this.Controls.Add(this.Nlabel);
+            this.Controls.Add(this.PIlabel);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.CategoriaIdtextBox);
+            this.Controls.Add(this.IMDBtextBox);
+            this.Controls.Add(this.CalificaciontextBox);
+            this.Controls.Add(this.AnotextBox);
+            this.Controls.Add(this.DescripciontextBox);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.PeliculaIdtextBox);
+            this.Name = "AgregarForm";
+            this.Text = "Agregar Peliculas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox PeliculaIdtextBox;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.TextBox DescripciontextBox;
+        private System.Windows.Forms.TextBox AnotextBox;
+        private System.Windows.Forms.TextBox CalificaciontextBox;
+        private System.Windows.Forms.TextBox IMDBtextBox;
+        private System.Windows.Forms.TextBox CategoriaIdtextBox;
+        private System.Windows.Forms.Label PIlabel;
+        private System.Windows.Forms.Label Nlabel;
+        private System.Windows.Forms.Label Delabel;
+        private System.Windows.Forms.Label Alabel;
+        private System.Windows.Forms.Label Calabel;
+        private System.Windows.Forms.Label CIlabel;
+        private System.Windows.Forms.Label Imlabel;
     }
 }
