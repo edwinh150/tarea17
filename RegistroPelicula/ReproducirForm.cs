b29047b3389053671b5587a3ca89c157b8398b1b
edwinh150 <edwinh2410@gmail.com>
2015-10-17 11:27:17 -0400

    reproductor formulario

--- RegistroPelicula/ReproducirForm.cs
+++ RegistroPelicula/ReproducirForm.cs
@@ -16,21 +16,5 @@ namespace RegistroPelicula
         {
             InitializeComponent();
         }
-
-
-        private void Playbutton_Click(object sender, EventArgs e)
-        {
-            PeliculasaxWindowsMediaPlayer.Ctlcontrols.play();
-        }
-
-        private void Pausebutton_Click(object sender, EventArgs e)
-        {
-            PeliculasaxWindowsMediaPlayer.Ctlcontrols.pause();
-        }
-
-        private void Stopbutton_Click(object sender, EventArgs e)
-        {
-            PeliculasaxWindowsMediaPlayer.Ctlcontrols.stop();
-        }
     }
 }
